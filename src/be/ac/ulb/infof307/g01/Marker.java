eadbe157b4db43f6b0fde182ba77ea2df537df98
Detobel36 <detobel36@phoenix-rebirth.fr>
2017-03-08 15:33:24 +0100

    Create Marker (and test) And add imagePath in Pokemon (+ test)

---
+++ src/be/ac/ulb/infof307/g01/Marker.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ac.ulb.infof307.g01;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author remy
+ */
+public class Marker {
+    
+    private final Pokemon _pokemon;
+    private final Coordinate _coordinate;
+    private Timestamp _timestamp;
+    
+    public Marker(Pokemon pokemon, Coordinate coordinate) {
+        _pokemon = pokemon;
+        _coordinate = coordinate;
+        
+        Long currentTime = System.currentTimeMillis();
+        _timestamp = new Timestamp(currentTime);
+    }
+    
+    public void setTimestamp(Timestamp newTimestamp) {
+        _timestamp = newTimestamp;
+    }
+    
+    public String getPathImage() {
+        return _pokemon.getPathImage();
+    }
+    
+}
---
+++ test/MarkerTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import be.ac.ulb.infof307.g01.Coordinate;
+import be.ac.ulb.infof307.g01.Marker;
+import be.ac.ulb.infof307.g01.Pokemon;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author remy
+ */
+public class MarkerTest {
+    
+    public MarkerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void test_getPathImage() {
+        Pokemon testPokemon = new Pokemon("test", "typeTest");
+        Coordinate testCoordinate = new Coordinate(69, 42);
+        
+        Marker testMarker = new Marker(testPokemon, testCoordinate);
+        assertEquals(testMarker.getPathImage(), testPokemon.getPathImage());        
+    }
+    
+}
