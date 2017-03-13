a9e118db4cd55e388cbf53d12680ac8fd1974a6e
Detobel36 <detobel36@hotmail.fr>
2017-03-13 15:48:14 +0100

    Center popup on parent (with @robin)

--- src/be/ac/ulb/infof307/g01/gui/PinPopUp.java
+++ src/be/ac/ulb/infof307/g01/gui/PinPopUp.java
@@ -11,8 +11,6 @@ import java.text.SimpleDateFormat;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
