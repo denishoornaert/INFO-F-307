84320f183c91d0a219d8539d84dfb75f9da12f5d
denishoornaert <dhoornae@ulb.ac.be>
2017-03-15 19:17:58 +0100

    add the skelleton of the controller that will communicate with the research combobox in the NewMarkerPopUp

---
+++ src/be/ac/ulb/infof307/g01/ResearchController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ac.ulb.infof307.g01;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hoornaert
+ */
+public class ResearchController {
+ 
+    public ResearchController() {}
+    
+    /*
+    * Method that aims to return the set of pokemon for which the name match the
+    * pattern given in parameters
+    *
+    * @parameters the pattern
+    * @return the list of pokemon names that matches the pattern
+    */
+    public ArrayList<String> getPokemonByName(String researchpattern) {
+        return new ArrayList<String>();
+    }
+    
+}
