5c90eb066c881deffc3aa7e4e37037dfeac00cc5
denishoornaert <dhoornae@ulb.ac.be>
2017-03-17 14:43:57 +0100

    add method that send return an array of string that contains a given sub-string

--- src/be/ac/ulb/infof307/g01/PokemonListModel.java
+++ src/be/ac/ulb/infof307/g01/PokemonListModel.java
@@ -6,6 +6,7 @@
 package be.ac.ulb.infof307.g01;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,19 +14,23 @@ import java.util.ArrayList;
  */
 public class PokemonListModel {
     
-    String[] _allPokemonName;
+    ArrayList<String> _allPokemonNames;
 
-    public PokemonListModel() {
-        _all
-        
+    public PokemonListModel(String ... names) {
+        _allPokemonNames = new ArrayList<String>();
+        for (String name : names) {
+            _allPokemonNames.add(name);
+        }
     }
     
-    public ArrayList<String> findPokemonFromPattern(String pattern) {
-        ArrayList<String> res = new ArrayList<>();
-        for (String str : _allPokemonName) {
-            res.add(str);
+    public String[] findPokemonFromPattern(String pattern) {
+        List<String> res = new ArrayList<>();
+        for (String str : _allPokemonNames) {
+            if(str.contains(pattern)) {
+                res.add(str);
+            }
         }
-        return res;
+        return res.stream().toArray(String[]::new);
     }
     
 }
