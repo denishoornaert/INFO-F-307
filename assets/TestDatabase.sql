a2dc655d019ef8350799feed66220158fb091b69
NathanLiccardo <nathan.liccardo@ulb.ac.be>
2017-03-29 11:51:44 +0200

    Fill database with Pokemon and pokemon types
    The link table between pokemon and pokemon types still needs to be filled.
    
    We erased lines from TestDatabase.sql because it is the same script as
    Database.sql. To make the test database, just cat both scripts.

--- assets/TestDatabase.sql
+++ assets/TestDatabase.sql
@@ -1,41 +1,5 @@
 BEGIN TRANSACTION;
 
--- Create table
-CREATE TABLE "PokemonType" (
-	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`Name`	TEXT UNIQUE
-);
-
-CREATE TABLE "PokemonPokemonTypeLink" (
-	`TypeId`	INTEGER NOT NULL,
-	`PokemonId`	INTEGER NOT NULL,
-	PRIMARY KEY(`TypeId`,`PokemonId`)
-);
-
-CREATE TABLE `Pokemon` (
-	`Id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`Name`	TEXT NOT NULL UNIQUE,
-	`ImagePath`	TEXT NOT NULL
-);
-
-CREATE TABLE "Marker" (
-	`Id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`PokemonId`	INTEGER NOT NULL,
-	`X`	INTEGER NOT NULL,
-	`Y`	INTEGER NOT NULL,
-	`TimeStamp`	TEXT NOT NULL,
-	FOREIGN KEY(`PokemonId`) REFERENCES Pokemon
-);
-
-
--- Insert into PokemonType
-INSERT INTO `PokemonType` VALUES (1,'Electrik');
-INSERT INTO `PokemonType` VALUES (2,'Water');
-
--- Insert Pokemon
-INSERT INTO `Pokemon` VALUES (1,'Pikachu','path to pikachu');
-INSERT INTO `Pokemon` VALUES (2,'Mystherbe','path to Mystherbe');
-
 -- Insert a link between Pikachu and Electrik
 INSERT INTO `PokemonPokemonTypeLink` VALUES (1,1);
 
