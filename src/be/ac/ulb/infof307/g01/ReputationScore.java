d2908d357b23b203bb588ad44fe0763db7ee72c7
TheoVerhelst <tverhels@ulb.ac.be>
2017-03-30 19:54:24 +0200

    Remove the empty constructor for ReputationScore
    This constructor was never used, and thus has no reason of existence.
    I also renamed the attributes to plural, as we probably have more than
    one vote.

--- src/be/ac/ulb/infof307/g01/ReputationScore.java
+++ src/be/ac/ulb/infof307/g01/ReputationScore.java
@@ -1,31 +1,25 @@
 package be.ac.ulb.infof307.g01;
 
-/**
- * TODO: add description
- */
+/** Score of reputation of a marker, as down and upvotes. */
 public class ReputationScore {
     
-    private int _upVote;
-    private int _downVote;
-
-    public ReputationScore() {
-        this(33, 17);
-    }
+    private int _upVotes;
+    private int _downVotes;
     
     public ReputationScore(int up, int down) {
-        _upVote = up;
-        _downVote = down;
+        _upVotes = up;
+        _downVotes = down;
     }
     
     public int getScore() {
-        return _upVote-_downVote;
+        return _upVotes - _downVotes;
     }
 
     void vote(ReputationVoteModel reputationVote) {
         if(reputationVote == ReputationVoteModel.UP) {
-            _upVote++;
+            _upVotes++;
         } else if (reputationVote == ReputationVoteModel.DOWN) {
-            _downVote++;
+            _downVotes++;
         }
     }
     
