d5717d772646d5b86e0ab15981ef1d1c5fe8f6ef
denishoornaert <dhoornae@ulb.ac.be>
2017-04-17 10:18:35 +0200

    New class PokemonPanel that will display a set of PokemonLabel. A pokemonLabel is a paire of photo and label (which display the pokemon name)

---
+++ src/be/ac/ulb/infof307/g01/view/PokemonLabel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ac.ulb.infof307.g01.view;
+
+import be.ac.ulb.infof307.g01.model.PokemonModel;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+/**
+ *
+ * @author hoornaert
+ */
+public class PokemonLabel extends BorderPane {
+    
+    private ImageView _image;
+    private Label _pokemonName;
+    
+    public PokemonLabel(PokemonModel pokemon) {
+        initWidgets(pokemon);
+        initStyle();
+        placeWidgets();
+    }
+
+    private void initWidgets(PokemonModel pokemon) {
+        _image = new ImageView(new Image(pokemon.getImagePath()));
+        _pokemonName = new Label(pokemon.getName());
+    }
+
+    private void initStyle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void placeWidgets() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/be/ac/ulb/infof307/g01/view/PokemonPanel.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ac.ulb.infof307.g01.view;
+
+import be.ac.ulb.infof307.g01.model.PokemonModel;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author hoornaert
+ */
+public class PokemonPanel extends StackPane {
+    
+    ScrollPane _scroll;
+    FlowPane _flow;
+    
+    public PokemonPanel() {
+        this(PokemonModel.getPokemonByName("Aggron"),
+            PokemonModel.getPokemonByName("Amaura"),
+            PokemonModel.getPokemonByName("Aggron"),
+            PokemonModel.getPokemonByName("Amaura"),
+            PokemonModel.getPokemonByName("Aggron"),
+            PokemonModel.getPokemonByName("Amaura"),
+            PokemonModel.getPokemonByName("Aggron"),
+            PokemonModel.getPokemonByName("Amaura"),
+            PokemonModel.getPokemonByName("Aggron"),
+            PokemonModel.getPokemonByName("Amaura"),
+            PokemonModel.getPokemonByName("Aggron"),
+            PokemonModel.getPokemonByName("Amaura"),
+            PokemonModel.getPokemonByName("Archen"));
+    }
+    
+    public PokemonPanel(PokemonModel... pokemons) {
+        super();
+        initWidgets(pokemons);
+        initStyle();
+        placeWidgets();
+    }
+
+    private void initWidgets(PokemonModel... pokemons) {
+        _scroll = new ScrollPane();
+        _flow = new FlowPane();
+        for (PokemonModel pokemon : pokemons) {
+            _flow.getChildren().add(new Button(pokemon.getName()));
+        }
+    }
+
+    private void initStyle() {
+        _flow.setAlignment(Pos.CENTER);
+        _flow.setHgap(10);
+        _flow.setVgap(10);
+        _scroll.setFitToWidth(true);
+        _scroll.setFitToHeight(true);
+    }
+
+    private void placeWidgets() {
+        _scroll.setContent(_flow);
+        this.getChildren().add(_scroll);
+    }
+    
+}
