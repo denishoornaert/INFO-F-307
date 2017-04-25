655a9e56c948e80f4ab795b7dcf4f0fa045d2526
denishoornaert <dhoornae@ulb.ac.be>
2017-04-25 19:21:09 +0200

    Refactor : now controllers call directly the user configuration

--- src/client/src/be/ac/ulb/infof307/g01/client/Main.java
+++ src/client/src/be/ac/ulb/infof307/g01/client/Main.java
@@ -2,6 +2,7 @@ package be.ac.ulb.infof307.g01.client;
 
 import be.ac.ulb.infof307.g01.client.controller.AuthenticationController;
 import be.ac.ulb.infof307.g01.client.controller.MapController;
+import be.ac.ulb.infof307.g01.client.model.Configuration;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
@@ -15,19 +16,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
   
     private static StackPane _layout;
-    private static Stage _stage;
-    
-    /** Path to application database. */
-    private static final String DATABASE_PATH = "../../assets/Database.db";
-    
-    /**
-     * Path to application tests  database.
-     * 
-     * Note: another database is needed for tests in order to not pollute the
-     * initial one in case any database test goes wrong
-     */
-    private static final String TEST_DATABASE_PATH = "../../assets/TestDatabase.db";
-    
+    private static Stage _stage;    
     private Scene _scene;
 
     @Override
@@ -43,7 +32,7 @@ public class Main extends Application {
         new MapController();
         
         _scene = new Scene(_layout);
-        _scene.getStylesheets().add(new File("../../assets/bootstrap.css").toURI().toString());
+        _scene.getStylesheets().add(Configuration.getInstance().getStylePath());
         stage.setScene(_scene);
         
         stage.show();
@@ -62,7 +51,7 @@ public class Main extends Application {
     
     /** Loads and inits the database. */
     private static void loadDatabase() throws SQLException, FileNotFoundException {
-        new DatabaseModel(getDatabasePath());
+        new DatabaseModel(Configuration.getInstance().getDataBasePath());
     }
     
     public static void main(String[] args) {
@@ -74,22 +63,6 @@ public class Main extends Application {
             return;
         }
         launch(args);
-    }  
-    
-    /**
-     * Returns the path of the application database.
-     * @return the path to the database to be used in the application
-     */
-    public static String getDatabasePath() {
-        return DATABASE_PATH;
-    }
-    
-    /**
-     * Returns the path or the tests database.
-     * @return the path of the database to be used in the tests
-     */
-    public static String getTestDatabasePath() {
-        return TEST_DATABASE_PATH;
     }
 
 }
