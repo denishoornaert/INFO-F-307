09810421541a63a9ed304a17f443f31bbcacddf1
denishoornaert <dhoornae@ulb.ac.be>
2017-04-26 17:58:46 +0200

    refacto : test moved

--- test/be/ac/ulb/infof307/g01/client/model/db/MarkerDatabaseModelTest.java
+++ test/be/ac/ulb/infof307/g01/client/model/db/MarkerDatabaseModelTest.java
@@ -2,7 +2,6 @@ package be.ac.ulb.infof307.g01.client.model.db;
 
 import be.ac.ulb.infof307.g01.client.Main;
 import be.ac.ulb.infof307.g01.client.model.DatabaseModel;
-import be.ac.ulb.infof307.g01.client.model.MarkerDatabaseModel;
 import be.ac.ulb.infof307.g01.client.model.MarkerModel;
 import be.ac.ulb.infof307.g01.client.model.PokemonModel;
 import be.ac.ulb.infof307.g01.client.model.PokemonTypeModel;
@@ -14,10 +13,11 @@ import java.util.ArrayList;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 import org.junit.*;
+import be.ac.ulb.infof307.g01.client.model.MarkerQueryModel;
 
 public class MarkerDatabaseModelTest {
     
-    private static MarkerDatabaseModel _database;
+    private static MarkerQueryModel _database;
     private MarkerModel _markerToInsert;
     
     @BeforeClass
