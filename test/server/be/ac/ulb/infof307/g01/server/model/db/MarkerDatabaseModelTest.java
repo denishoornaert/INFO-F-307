df82095fd357252c7631eabac20684c671884d3b
Stanislas Gueniffey <sgueniff@ulb.ac.be>
2017-05-04 18:10:58 +0200

    Fix marker creation test - heavy use of database test setups
    We need to call loadAllPokemon() on the database before being able to create PokemonSendableModel and MarkerSendableModel instances.

--- test/server/be/ac/ulb/infof307/g01/server/model/db/MarkerDatabaseModelTest.java
+++ test/server/be/ac/ulb/infof307/g01/server/model/db/MarkerDatabaseModelTest.java
@@ -1,7 +1,6 @@
 package be.ac.ulb.infof307.g01.server.model.db;
 
 import be.ac.ulb.infof307.g01.common.model.MarkerSendableModel;
-import be.ac.ulb.infof307.g01.common.model.PokemonSendableModel;
 import be.ac.ulb.infof307.g01.common.model.UserSendableModel;
 import java.sql.Timestamp;
 import static junit.framework.TestCase.assertEquals;
@@ -10,33 +9,32 @@ import org.junit.Test;
 
 public class MarkerDatabaseModelTest extends AbstractDatabaseTest {
     private final String _markerUsername = "bidon";
+    private final String _markerPokemonName = "Arceus";
     private final UserSendableModel _markerUser;
-    private final MarkerSendableModel _markerToInsert;
+    private MarkerSendableModel _markerToInsert;
     
     public MarkerDatabaseModelTest() {
         super();
         _markerUser = new UserSendableModel(_markerUsername, "mail", "pass");
-        
-        final int id = 0;
-        final double latitude = 250;
-        final double longitude = 500;
-        final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        PokemonSendableModel arceus = PokemonSendableModel.getPokemonByName("Arceus");
-        final String pokemonName = arceus.getName();
-        final int upVotes = 0;
-        final int downVotes = 0;
-        final int lifePoint = 0;
-        final int attack = 0;
-        final int defense = 0;
-        _markerToInsert = new MarkerSendableModel(id, _markerUsername, pokemonName, 
-                latitude, longitude, timestamp.getTime(), upVotes, 
-                downVotes,lifePoint,attack,defense);
     }
     
     @Before
     @Override
     public void setUp() {
         super.setUp();
+        
+        final int id = 0;
+        final double latitude = 250;
+        final double longitude = 500;
+        final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        final int upVotes = 0;
+        final int downVotes = 0;
+        final int lifePoint = 0;
+        final int attack = 0;
+        final int defense = 0;
+        _markerToInsert = new MarkerSendableModel(id, _markerUsername, _markerPokemonName, 
+                latitude, longitude, timestamp.getTime(), upVotes, 
+                downVotes, lifePoint, attack, defense);
         String token = "109283";
         boolean test = _database.signup(_markerUser, token);
         if (test) test = _database.confirmAccount(token);
