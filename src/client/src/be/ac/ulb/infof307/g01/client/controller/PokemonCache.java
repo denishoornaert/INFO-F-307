f243760b716df5d7806da2e4429e62064f9c81f2
TheoVerhelst <tverhels@ulb.ac.be>
2017-05-05 02:12:39 +0200

    Add Javadoc to PokemonCache

--- src/client/src/be/ac/ulb/infof307/g01/client/controller/PokemonCache.java
+++ src/client/src/be/ac/ulb/infof307/g01/client/controller/PokemonCache.java
@@ -13,10 +13,24 @@ import java.util.Map;
  * Singleton which holds the client pokemon information sent by the server.
  * Since these pokemons are not subject to change, we just need to store them
  * once, and all classes needing info about a pokemon ask us.
+ * 
+ * The singleton instance is filled by ServerQueryController when this latter is
+ * constructed.
  */
 public class PokemonCache {
+    /**
+     * Singleton instance.
+     */
     private static PokemonCache _instance = null;
+    
+    /**
+     * All registered pokemons so far, sorted by name.
+     */
     private Map<String, PokemonModel> _allPokemons = new HashMap();
+    
+    /**
+     * All registered pokemon types so far, sorted by name.
+     */
     private Map<String, PokemonTypeModel> _allPokemonTypes = new HashMap();
 
     private PokemonCache() {
@@ -29,13 +43,23 @@ public class PokemonCache {
         return _instance;
     }
     
+    /**
+     * Replaces the cache of pokemons with this given one.
+     * @param allPokemons The new list of pokemons used to replace the previous
+     * one.
+     */
     public void loadAllPokemons(List<PokemonSendableModel> allPokemons) {
-        clearAllPokemons();
+        _allPokemons.clear();
         for (PokemonSendableModel pokemon : allPokemons) {
             _allPokemons.put(pokemon.getName(), new PokemonModel(pokemon));
         }
     }
     
+    /**
+     * Replaces the cache of pokemon types with this given one.
+     * @param allPokemonTypes The new list of pokemon types used to replace the
+     * previous one.
+     */
     public void loadAllPokemonTypes(List<PokemonTypeSendableModel> allPokemonTypes) {
         _allPokemonTypes.clear();
         for (PokemonTypeSendableModel pokemonType : allPokemonTypes) {
@@ -43,14 +67,31 @@ public class PokemonCache {
         }
     }
         
+    /**
+     * Returns the list of all names of all cached pokemons.
+     * @return the list of all names of all cached pokemons.
+     * ^^^ Look how these lines are syncing up, this is beautiful. ^^^
+     */
     public ArrayList<String> getAllPokemonNames() {
         return new ArrayList(_allPokemons.keySet());
     }
     
+    /**
+     * Returns a list of all cached pokemons types.
+     * @return a list of all cached pokemons types.
+     * ^^^ Same remark as for getAllPokemonNames ^^^
+     */
     public ArrayList<PokemonTypeModel> getAllPokemonTypes() {
         return new ArrayList(_allPokemonTypes.values());
     }
     
+    /**
+     * Retrieves the PokemonModel instance by its name.
+     * 
+     * @param pokemonName the name of the searched pokemon.
+     * @return the PokemonModel instance.
+     * @throws RuntimeException if no pokemon with such name has been found.
+     */
     public PokemonModel getPokemonByName(String pokemonName) {
         PokemonModel result = _allPokemons.get(pokemonName);
         if(result == null) {
@@ -60,10 +101,11 @@ public class PokemonCache {
     }
     
     /**
-     * Returns the pokemonTypeModel instance by name
+     * Retrieves the pokemonTypeModel instance by its name.
      * 
-     * @param typeName the desired name
-     * @return the PokemonTypeModel instance or null if not found
+     * @param typeName the name of the searched type.
+     * @return the PokemonTypeModel instance.
+     * @throws RuntimeException if no type with such name has been found.
      */
     public PokemonTypeModel getPokemonTypeByTypeName(String typeName) {
         PokemonTypeModel result = _allPokemonTypes.get(typeName);
@@ -73,18 +115,9 @@ public class PokemonCache {
         return result;
     }
     
-    public void clearAllPokemons() {
-        _allPokemons.clear();
-    }
-    
-    public void clearAllPokemonTypes() {
-        _allPokemonTypes.clear();
-    }
-    
     /**
-     * Gets the number of loaded pokemon.
-     * 
-     * @return number of pokemon 
+     * Returns the number of loaded pokemon.
+     * @return the number of loaded pokemon.
      */
     public int getNumberLoadedPokemons(){
         return _allPokemons.size();
