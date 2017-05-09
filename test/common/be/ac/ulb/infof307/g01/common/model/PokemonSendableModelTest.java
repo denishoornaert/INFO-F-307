5fc56dffe9c8b697d7cb547663b73f0aec49e3f3
Stanislas Gueniffey <sgueniff@ulb.ac.be>
2017-05-09 18:28:30 +0200

    Add test for PokemonSendableModel : no second type results in none type

---
+++ test/common/be/ac/ulb/infof307/g01/common/model/PokemonSendableModelTest.java
@@ -0,0 +1,20 @@
+package be.ac.ulb.infof307.g01.common.model;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Tests the PokemonSendableModel class.
+ */
+public class PokemonSendableModelTest extends TestCase {
+    static final String POKEMON_NAME = "Arceus";
+    static final String POKEMON_PATH = "arceus.png";
+    static final PokemonTypeSendableModel POKEMON_TYPE = new PokemonTypeSendableModel("FIRE");
+    
+    @Test
+    public void test_createModelWithOneTypeHasNoneSecondType() {
+        PokemonSendableModel pokemon = new PokemonSendableModel(
+            POKEMON_NAME, POKEMON_PATH, POKEMON_TYPE);
+        assertEquals(pokemon.getType()[1], PokemonTypeSendableModel.getNoneType());
+    }
+}
