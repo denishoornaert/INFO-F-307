4d9f1a9e00a84b83fa11e93dc9f3a218ab5eaece
davidabh@hotmail.com <davidabraham@172.19.21.185>
2017-05-09 18:57:18 +0200

    up some errors since MarkerQueryController with theo

--- src/client/src/be/ac/ulb/infof307/g01/client/controller/MessagePopUpController.java
+++ src/client/src/be/ac/ulb/infof307/g01/client/controller/MessagePopUpController.java
@@ -2,14 +2,25 @@ package be.ac.ulb.infof307.g01.client.controller;
 
 import be.ac.ulb.infof307.g01.client.view.MessagePopUp;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class MessagePopUpController extends AbstractPopUpController {
+
+    private static final Logger LOG = Logger.getLogger(MessagePopUpController.class.getName());
     
     public MessagePopUpController(Level severity, String message) throws InstantiationException {
         super();
-        // We don't use the view afterwards, just instanciate it.
         new MessagePopUp(this, severity, message);
+    }
+    
+    public static void createPopUpOrLog(Level severity, String message){
+        try {
+            new MessagePopUpController(severity, message);
+        } catch (InstantiationException ex) {
+                LOG.log(Level.WARNING, "This error couldn't be shown with a popup: {0}",
+                        message);
+        }
     }
     
     /**
